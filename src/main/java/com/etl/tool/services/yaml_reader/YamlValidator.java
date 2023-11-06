ecd0aaada344e205e5bc91d55f859961af2f9bd3
JoYousefShaban <yousefsha3bola2001@gmail.com>
2023-11-06 07:07:06 +0000

    Improved yaml file validations

--- src/main/java/com/etl/tool/services/yaml_reader/YamlValidator.java
+++ src/main/java/com/etl/tool/services/yaml_reader/YamlValidator.java
@@ -27,14 +27,25 @@ public class YamlValidator {
 
     @SuppressWarnings("squid:S2178")
     private static boolean isConnectionValid(ConnectionEntity connection, String connectionType) {
-        boolean isValid = isFieldValid(connection.getJdbcUrl(), "jdbcUrl", connectionType) &
-                isFieldValid(connection.getUsername(), "username", connectionType) &
-                isFieldValid(connection.getPassword(), "password", connectionType) &
-                isFieldValid(connection.getDriverClassName(), "driverClassName", connectionType) &
-                isFieldValid(connection.getHikari().getConnectionTimeout(), "connectionTimeout", connectionType) &
-                isFieldValid(connection.getHikari().getMaximumPoolSize(), "maximumPoolSize", connectionType);
+        boolean isValid = false;
 
-        log.info(ANSI.colour("The connection configuration of " + connectionType + " resulted as: " + (isValid ? "valid" : "invalid"), isValid ? ANSI.TEAL_BOLD : ANSI.RED_BOLD));
+        if (connection != null) {
+            isValid = isFieldValid(connection.getJdbcUrl(), "jdbcUrl", connectionType) &
+                    isFieldValid(connection.getUsername(), "username", connectionType) &
+                    isFieldValid(connection.getPassword(), "password", connectionType) &
+                    isFieldValid(connection.getDriverClassName(), "driverClassName", connectionType);
+
+            if (connection.getHikari() != null) {
+                isValid = isValid && isFieldValid(connection.getHikari().getConnectionTimeout(), "connectionTimeout", connectionType) &
+                        isFieldValid(connection.getHikari().getMaximumPoolSize(), "maximumPoolSize", connectionType);
+            } else {
+                log.error(ANSI.colour("Please enter the hikari values for " + connectionType + "  configuration", ANSI.RED_BOLD));
+            }
+        } else {
+            log.error(ANSI.colour("Please enter the " + connectionType + " configurations", ANSI.RED_BOLD));
+        }
+
+        log.info(ANSI.colour("The configuration for " + connectionType + " resulted as " + (isValid ? "valid" : "invalid"), isValid ? ANSI.TEAL_BOLD : ANSI.RED_BOLD));
         return isValid;
     }
 
@@ -48,7 +59,7 @@ public class YamlValidator {
 
     private static boolean isFieldValid(int field, String fieldName, String connectionType) {
         if (field == 0) {
-            log.error(ANSI.colour("Please enter the value of " + fieldName + " in: " + connectionType, ANSI.RED_BOLD));
+            log.error(ANSI.colour("Please enter the value of " + fieldName + " in: " + connectionType + " configuration", ANSI.RED_BOLD));
             return false;
         }
         return true;
